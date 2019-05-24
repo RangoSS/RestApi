1799f9cf36ca9fc4ad70f58e46bc96ef85dd3d51
RangoSS <tshidaisa@outlook.com>
2019-05-24 12:22:16 +0200

    rest api

---
+++ main.js
@@ -0,0 +1,17 @@
+function bookSearch(){
+	var search=document.getElementById('search').value
+	document.getElementById('results').innerHTML=""
+	console.log(search)
+    
+    //q on the url is asking
+	$.ajax({
+		url:"https://www.googleapis.com/books/v1/volumes?q=" +search,
+		dataType:"json",
+		success:function(data){
+			console.log(data)
+		},
+		type:'GET'
+	});
+}
+
+document.getElementById('button').addEventListener('click',bookSearch,false)
---
+++ test.js
@@ -0,0 +1,12 @@
+function bookSearch(){
+	var search=document.getElementById('search').value
+	console.log('my function is running');
+
+
+}
+
+document.getElementById('button').addEventListener('click',bookSearch(),false)
+
+
+
+
