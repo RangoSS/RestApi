b874fae178708d8f69c191da99d9e4a0395ff495
RangoSS <tshidaisa@outlook.com>
2019-05-24 14:42:07 +0200

    git here

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ function bookSearch(){
 		url:"https://www.googleapis.com/books/v1/volumes?q=" +search,
 		dataType:"json",
 		success:function(data){
-			console.log(data)
+			for (i=0; i<data.items.length;i++){
+				results.innerHTML +="<h2>" + data.items[i].volumeInfo.title + "<h2>"
+			}
 		},
 		type:'GET'
 	});
